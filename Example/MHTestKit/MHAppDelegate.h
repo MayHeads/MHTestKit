424c8a357f3d2ddf2e7f848fc31171dd7df3fd4b
秦光义 <qinguangyi@inke.cn>
2022-05-15 16:45:46 +0800

    Initial commit

---
+++ Example/MHTestKit/MHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MHAppDelegate.h
+//  MHTestKit
+//
+//  Created by MayHeads on 05/15/2022.
+//  Copyright (c) 2022 MayHeads. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/MHTestKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MHTestKit
+//
+//  Created by MayHeads on 05/15/2022.
+//  Copyright (c) 2022 MayHeads. All rights reserved.
+//
+
+@import UIKit;
+#import "MHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MHAppDelegate class]));
+    }
+}
